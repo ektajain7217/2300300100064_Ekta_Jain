67ab2f42e6aec8ee5668aecc7647cf35a7b8d6f5
ektajain7217 <152249843+ektajain7217@users.noreply.github.com>
2026-02-18 16:58:28 +0530

    Add array leaders program

--- twosum.cpp
+++ twosum.cpp
@@ -23,7 +23,7 @@ class sloution{
     public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        for( int i= 0; i<n; i++){
+        for(int i= 0; i<n; i++){
             for( int j = i+1; j<n; j++){
                 if(nums[i]+nums[j] == target){
                     return {i,j};
