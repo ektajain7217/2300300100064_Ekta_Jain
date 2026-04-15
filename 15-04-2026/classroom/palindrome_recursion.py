fc724be6f34a3da10c91ee5ffb51ab90bd001d8e
ektajain7217 <152249843+ektajain7217@users.noreply.github.com>
2026-04-15 10:12:18 +0530

    palindrome using recursion

---
+++ 15-04-2026/classroom/palindrome_recursion.py
@@ -0,0 +1,15 @@
+# writing a program to check if the string is palindrome using recursion
+def is_palindrome(s):
+    if len(s)<= 1:
+        return True
+    if s[0]!= s[-1]:
+        return False
+    return is_palindrome(s[1:-1])
+
+string = input("Enter a string: ")
+if is_palindrome(string):
+    print(f"{string} is a palindrome.")
+else: 
+    print(F"{string} is not a palindrome.")
+
+    
